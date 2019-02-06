58e76409a607604bf5f0c04b8190bf73b2bdf844
becca <becca@rosingledeMacBook-Air.local>
2019-02-06 17:48:40 +0100

    First commit

---
+++ chapter 5.js
@@ -0,0 +1,19 @@
+// Chapitre 5 ajouter des conditions
+
+// exercice 1 changez les valeurs des variables temperature et formule
+const temperature = 0;
+if (temperature <= 0) {
+	console.log("Il gèle");
+	}
+else {
+	console.log("Il fait pas si froid");
+}
+
+const formule = "abracadabra";
+if (formule === "abracadabra"){
+	console.log("C'est la formule magique !");
+}
+else {
+	console.log("Ce n'est pas la bonne formule");
+}
+// exercice 2
