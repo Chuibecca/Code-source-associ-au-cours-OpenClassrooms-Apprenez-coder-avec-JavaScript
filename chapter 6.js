80d9ef383f7971e66e7c29414f8d53d453813861
becca <becca@rosingledeMacBook-Air.local>
2019-02-08 11:59:00 +0100

    add a line to send a PR

--- chapter 6.js
+++ chapter 6.js
@@ -1,8 +1,69 @@
-// Le boucle while
-console.log("Début du programme");
-let nombre = 1;
-while (nombre <= 5) {
-	console.log(nombre);
-	nombre++;
+// Tournez manege
+const nbTours = 10;
+
+console.log("Le manège démarre");
+
+// Solution avec for
+for (let tour = 1; tour <= nbTours; tour++) {
+	console.log(`C'est le tour numéro ${tour}`);
 }
-console.log("Fin du programme");
+
+// Solution alternative avec un while
+let tour = 1;
+while (tour <= nbTours) {
+  console.log(`C'est le tour numéro ${tour}`);
+  tour++;
+}
+
+console.log("Le manège s'arrête");
+
+
+
+
+
+// Table de multiplication
+const nombre = 7; // Faites varier cette variable entre 1 et 10
+
+console.log(`Table de multiplication de ${nombre}`);
+
+// Solution avec un for
+for (let i = 1; i <= 10; i++) {
+  console.log(`${nombre} x ${i} = ${nombre * i}`);
+}
+
+// Solution avec un while
+let i = 1;
+while (i <= 10) {
+  console.log(`${nombre} x ${i} = ${nombre * i}`);
+  i++;
+}
+
+
+
+
+// FizzBuzz
+
+//  Solution avec for
+for (let nombre =1; nombre <= 100; nombre ++){
+	if (nombre % 3 === 0 && nombre % 5 === 0)
+	console.log("FizzBuzz");
+	else if (nombre % 3 === 0)
+	console.log("Fizz");
+	else
+	console.log(nommbre);
+}
+
+
+// function direBonjour() {
+//   console.log("Bonjour !");
+// }
+
+// console.log("Début du programme");
+// direBonjour();
+// console.log("Fin du programme");
+
+// function direBonjour() {
+//   return "Bonjour !";
+// }
+
+// console.log(direBonjour()); // "Bonjour !"
